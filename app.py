f4d1c5d696aff16bb5bd990a79c73259d7c04786
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-09 14:26:28 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import openai
+import streamlit as st
+
+def generate_response(prompt):
+    completion = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.6,
+    )
+    message = completion.choices[0].text
+    return message
+
+def main():
+    st.title("ChatGPT-Sayon Web App")
+    
+    # Ask the user for their OpenAI API key
+    api_key = st.text_input("Enter your OpenAI API key:")
+    openai.api_key = api_key
+    
+    # Storing the chat
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+    
+    user_input = st.text_input("You:")
+    
+    if user_input:
+        output = generate_response(user_input)
+        # Store the message in the chat history
+        st.session_state['chat_history'].append({'user_input': user_input, 'bot_output': output})
+    
+    if st.session_state['chat_history']:
+        for i in range(len(st.session_state['chat_history'])):
+            chat = st.session_state['chat_history'][i]
+            st.write("You: " + chat['user_input'])
+            st.write("Bot: " + chat['bot_output'])
+
+if __name__ == "__main__":
+    main()
